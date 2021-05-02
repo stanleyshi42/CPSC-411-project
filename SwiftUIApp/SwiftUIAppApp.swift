fc1502d1ca2a88d6f96c0509e4d77c8f676dc563
Stanley Shi <stanleyshi42@gmail.com>
2021-05-02 13:50:30 -0700

    Add files via upload

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by James Shen on 4/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
